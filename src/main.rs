aaeecc1a94135578ab6f5dacdb133e978dc56ab0
Miccah Castorina <m.castorina93@gmail.com>
2020-12-14 21:56:35 -0600

    Implement the standard algorithm
    This algorithm works by reading N/2 amount of data at a time into a
    buffer of size N. If the pattern is in the buffer, we find the closest
    newline byte and stop printing there. Otherwise, we print the first half
    of the buffer and shift everything up.
    
    The advantage of this algorithm is that it works efficiently on very
    large files. We only use N bytes of buffer regardless of the filesize.
    
    The disadvantage is that it might not be correct for files with lines
    longer than N/2.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use clap_v3::{App, Arg};
+use regex::bytes::Regex;
+use std::fs::File;
+use std::io::{self, Write};
+
+const BUF_SIZE: usize = 4096;
 
 fn main() {
     let matches = App::new("heavi")
@@ -24,17 +29,39 @@ fn main() {
         )
         .get_matches();
 
+    // Get arguments
+    // ------------------------------------------------------------
     // Calling .unwrap() is safe here because "PATTERN" is required
-    println!(
-        "Using input pattern: {}",
-        matches.value_of("PATTERN").unwrap()
-    );
+    let pattern = matches.value_of("PATTERN").unwrap();
 
-    if let Some(file) = matches.value_of("FILE") {
-        println!("Using input file: {}", file);
-    }
+    let mut stream = match matches.value_of("FILE") {
+        Some(file) => Box::new(File::open(file).unwrap()) as Box<dyn io::Read>, // FIXME: unwrap
+        None => Box::new(io::stdin()) as Box<dyn io::Read>,
+    };
 
-    if matches.is_present("invert-match") {
-        println!("Inverting");
+    let invert = matches.is_present("invert-match");
+
+    // Iterate through stream
+    // ------------------------------------------------------------
+    let re = Regex::new(pattern).unwrap(); // FIXME: unwrap
+    let mut stdout = io::stdout();
+
+    let mut dbuf = [0; BUF_SIZE];
+    let mut n = stream.read(&mut dbuf).unwrap(); // FIXME: unwrap
+    loop {
+        if n == 0 {
+            break;
+        }
+        if let Some(m) = re.find(&dbuf) {
+            let sect = &dbuf[..m.start()];
+            if let Some(idx) = sect.iter().rev().position(|&r| r == 10) {
+                stdout.write(&dbuf[..m.start() - idx]).unwrap(); // FIXME: unwrap
+            }
+            break;
+        }
+        stdout.write(&dbuf[..BUF_SIZE / 2]).unwrap(); // FIXME: unwrap
+
+        dbuf.copy_within(BUF_SIZE / 2.., 0);
+        n = stream.read(&mut dbuf[BUF_SIZE / 2..]).unwrap(); // FIXME: unwrap
     }
 }
