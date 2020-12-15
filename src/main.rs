bd7cc6867376d3f217d8639d014df29beb2428a5
Miccah Castorina <m.castorina93@gmail.com>
2020-12-15 00:01:41 -0600

    Implement inverse
    This algorithm was less planned out, so it is a bit messy. It appears to
    work from manual testing.

--- src/main.rs
+++ src/main.rs
@@ -48,20 +48,75 @@ fn main() {
 
     let mut dbuf = [0; BUF_SIZE];
     let mut n = stream.read(&mut dbuf).unwrap(); // FIXME: unwrap
-    loop {
-        if n == 0 {
-            break;
-        }
-        if let Some(m) = re.find(&dbuf) {
-            let sect = &dbuf[..m.start()];
-            if let Some(idx) = sect.iter().rev().position(|&r| r == 10) {
-                stdout.write(&dbuf[..m.start() - idx]).unwrap(); // FIXME: unwrap
+    if !invert {
+        while n != 0 {
+            if let Some(m) = re.find(&dbuf) {
+                let sect = &dbuf[..m.start()];
+                // last newline before pattern
+                if let Some(idx) = sect.iter().rev().position(|&r| r == 10) {
+                    stdout.write(&dbuf[..m.start() - idx]).unwrap(); // FIXME: unwrap
+                }
+                break;
             }
-            break;
-        }
-        stdout.write(&dbuf[..BUF_SIZE / 2]).unwrap(); // FIXME: unwrap
+            stdout.write(&dbuf[..BUF_SIZE / 2]).unwrap(); // FIXME: unwrap
 
-        dbuf.copy_within(BUF_SIZE / 2.., 0);
-        n = stream.read(&mut dbuf[BUF_SIZE / 2..]).unwrap(); // FIXME: unwrap
+            // iterate through the file
+            dbuf.copy_within(BUF_SIZE / 2.., 0);
+            n = stream.read(&mut dbuf[BUF_SIZE / 2..]).unwrap(); // FIXME: unwrap
+        }
+    } else {
+        // find the match
+        while dbuf[0] != 0 {
+            let idx = match re.find(&dbuf) {
+                Some(m) => Some(m.end()),
+                None => None,
+            };
+            if let Some(idx) = idx {
+                // shift out the match
+                dbuf.copy_within(idx.., 0);
+                n = stream.read(&mut dbuf[BUF_SIZE - idx..]).unwrap(); // FIXME: unwrap
+                if n < idx {
+                    // fill with 0
+                    for i in BUF_SIZE-idx..BUF_SIZE {
+                        dbuf[i] = 0;
+                    }
+                }
+                break;
+            }
+            // iterate through the file
+            dbuf.copy_within(BUF_SIZE / 2.., 0);
+            n = stream.read(&mut dbuf[BUF_SIZE / 2..]).unwrap(); // FIXME: unwrap
+        }
+
+        // find the newline
+        while dbuf[0] != 0 {
+            let idx = dbuf.iter().position(|&r| r == 10);
+            if let Some(idx) = idx {
+                // shift out the newline
+                dbuf.copy_within(idx+1.., 0);
+                n = stream.read(&mut dbuf[BUF_SIZE-(idx+1)..]).unwrap(); // FIXME: unwrap
+                if n < idx+1 {
+                    // fill with 0
+                    for i in BUF_SIZE-(idx+1)..BUF_SIZE {
+                        dbuf[i] = 0;
+                    }
+                }
+                break;
+            }
+            n = stream.read(&mut dbuf).unwrap(); // FIXME: unwrap
+            if n < BUF_SIZE {
+                // fill with 0
+                for i in n..BUF_SIZE {
+                    dbuf[i] = 0;
+                }
+            }
+        }
+
+        // print the rest of the file
+        stdout.write(&dbuf).unwrap(); // FIXME: unwrap
+        while n != 0 {
+            n = stream.read(&mut dbuf).unwrap(); // FIXME: unwrap
+            stdout.write(&dbuf[..n]).unwrap(); // FIXME: unwrap
+        }
     }
 }
